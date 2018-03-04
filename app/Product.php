99f50c60d476327db3fa6b3976e859da97c7cfd5
Myris <myriszadip@gmail.com>
2018-03-04 19:33:02 +0100

    modify product and Group porduct

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+	public function groups()
+    {
+    	return $this->belongsToMany('App\Group');
+    }
+
+    public function presentPrice()
+    {
+        return '$'.number_format($this->price / 100, 2);
+    }
 }
