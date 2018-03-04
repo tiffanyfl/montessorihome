8aa0c16124b28bbe587104614f20be8d86fb63df
Myris <myriszadip@gmail.com>
2018-03-04 15:27:57 +0100

    front's part : products, categories (groups) and pagination

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i < 4; $i++){
+            Product::create([
+                'name' => 'Kitchen '.$i,
+                'slug' => 'kitchen-'.$i,
+                'details' => 'plastic',
+                'price' => rand(85, 95),
+                'description' => 'Lorem'.$i.' ipsum dolor sit amet'
+            ])->groups()->attach(1);
+        }
+
+        $product = Product::find(1);
+        $product->groups()->attach(2);
+
+        for($i = 1; $i < 4; $i++){
+            Product::create([
+                'name' => 'Living Room '.$i,
+                'slug' => 'living-room-'.$i,
+                'details' => 'plastic',
+                'price' => rand(85, 95),
+                'description' => 'Lorem'.$i.' ipsum dolor sit amet'
+            ])->groups()->attach(2);
+        }
+    }
+}
---
+++ resources/views/common.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Montessori Home</title>
+        <link href="https://fonts.googleapis.com/css?family=Muli|Quicksand" rel="stylesheet">
+
+        {!! Html::style('https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css') !!}
+        {!! Html::style('https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css') !!}
+
+        <!--[if lt IE 9]>
+            {{ Html::style('https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js') }}
+            {{ Html::style('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') }}
+        <![endif]-->
+        <style>
+
+        textarea { resize: none; }
+        body{
+          font-family: 'Muli', sans-serif;
+          margin: 0 auto;
+          }
+
+        h1, h2, h3 {
+          font-family: 'Quicksand', sans-serif;
+          color: #93D3D2;
+        }
+        .logo {
+          margin-top: 2vh;
+          margin-bottom: 7vh;
+          width: 22%;
+        }
+
+        .row-concept {
+          margin-bottom: 5vh;
+        }
+
+        .concept-img {
+          width: 90%;
+        }
+
+        .btn-montessori {
+          background-color: #93D3D2;
+          color: #fff;
+          font-family: 'Quicksand', sans-serif;
+        }
+
+        .row-newsletter {
+          margin-bottom: 50px;
+        }
+
+        .confirm{
+          margin-top: 30px;
+        }
+
+        .footer {
+          background-color: #93D3D2;
+          position: absolute;
+          width: 100%;
+          height: 60px;
+        }
+
+        .footer-txt {
+          padding-top: 15px;
+          color: #fff;
+        }
+
+        @media screen and (min-width: 769px) {
+        .footer {
+          /*bottom: 0;*/
+        }
+      }
+
+      @media screen and (min-width: 769px) and (max-width: 991px) {
+      .concept-h2{
+        margin-top: 0;
+      }
+    }
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            
+            @yield('contenu')
+            
+        </div>
+  <footer class="footer">
+    <div class="container">
+      <div class="col-sm-offset-3 col-sm-6 text-center footer-txt">Montessori Home - 2018</div>
+    </div>
+  </footer>
+</body>
+</html>
