bfed70e00e77781ca4e4133cdb61a84c31fba5ca
Myris <myriszadip@gmail.com>
2018-05-06 17:00:54 +0200

    format mobile search page

--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -35,30 +35,23 @@
       <span>Recherche</span>
   @endcomponent
 
-  
+
     <h1>Résultats de la recherche</h1>
     <p>{{ $products->total() }} résultat(s) pour "{{ request()->input('query') }}"</p>
 
-    <table class="table table-bordered table-striped">
-      <thead>
-          <tr>
-              <th>Nom</th>
-              <th>Details</th>
-              <th>Description</th>
-              <th>Prix</th>
-          </tr>
-      </thead>
-      <tbody>
-          @foreach ($products as $product)
-              <tr>
-                  <th><a href="{{ route('shop.show', $product->slug) }}">{{ $product->name }}</a></th>
-                  <td>{{ $product->details }}</td>
-                  <td>{{ str_limit($product->description, 80) }}</td>
-                  <td>{{ $product->presentPrice() }}</td>
-              </tr>
-          @endforeach
-      </tbody>
-    </table>
+    <section class="search-product-result text-center">
+      @foreach ($products as $product)
+        <figure>
+            <a href="{{ route('shop.show', $product->slug) }}">
+              <img src="{{ productImage($product->image) }}" width="150" height="100" alt="{{ $product->name }}">
+            </a>
+            <figcaption>
+              <a href="{{ route('shop.show', $product->slug) }}">{{ $product->name }}</a><br>
+              <p>{{ str_limit($product->description, 80) }} <br> {{ $product->presentPrice() }}</p>
+            </figcaption>
+        </figure>
+      @endforeach
+    </section>
 
     <!-- pagination -->
     <div class="text-center">
